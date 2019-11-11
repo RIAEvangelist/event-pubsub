92e8a2ba8fb12eecd7170c90463ddf0657dd4401
Timbo White <>
2019-11-11 14:40:51 +0000

    fix #19

--- event-pubsub.js
+++ event-pubsub.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const semver = require('semver');
+
 let EventPubSub = require('./es5');
-if(process.version[1]>5){
+if(semver.gt(process.versions.node, '5.0.0')){
     EventPubSub = require('./es6');
 }
 
