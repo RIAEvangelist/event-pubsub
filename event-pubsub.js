c0295ca7b69588fe304401b0acf81469d97ef6ca
RIAEvangelist <brandon@diginow.it>
2019-12-06 00:04:24 -0800

    privatized and protected architecture and members to harden against member collisions

--- event-pubsub.js
+++ event-pubsub.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const emit=require('./members/#emit.js'),
+    emit$=require('./members/#emit$.js'),
+    off=require('./members/#off.js'),
+    on=require('./members/#on.js'),
+    once=require('./members/#once.js');
+
 class EventPubSub {
     //locking down the interface for extension
     //making things protected vs private (hack 4 now)
